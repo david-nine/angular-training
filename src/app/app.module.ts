3ef48e9e59d7315879fc55448a12eb2d233c6ec6
David Hildebrandt <davihildebran@gmail.com>
2022-12-30 15:08:57 -0300

    Update to angular 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {CoreModule} from "./core.module";
 import {SharedModule} from "./shared/shared.module";
-import {StoreModule} from "@ngrx/store";
+//import {StoreModule} from "@ngrx/store";
 
 @NgModule({
   declarations: [
@@ -19,7 +19,7 @@ import {StoreModule} from "@ngrx/store";
     HttpClientModule,
     CoreModule,
     SharedModule,
-    StoreModule,
+    //StoreModule,
     AppRoutingModule
   ],
   bootstrap: [AppComponent]
